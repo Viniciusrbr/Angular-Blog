b32c22623bb840d5657af3da7d5c543f5f79da56
Viniciusrbr <viniciusgoes159@gmail.com>
2022-11-12 20:29:27 -0300

    content criado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const DataFake = [
+    {
+        "id":"1",
+        "title":"random card tittle",
+        "description": "descricao de um negocio aqui",
+        "photoCover": "https://cdn.pixabay.com/photo/2015/09/17/16/40/book-944462_960_720.jpg"
+    },
+    {
+        "id":"2",
+        "title":"Novidades do angular 14",
+        "description": "descricao de um negocio aqui",
+        "photoCover": "https://www.bacancytechnology.com/blog/wp-content/uploads/2022/06/Angular-14-min.jpg"
+    }
+]
